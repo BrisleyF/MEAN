f6809a39a17b0fc6b3516dcc729b21c1431df96d
Brisley <fajardobrisley@gmail.com>
2024-06-06 13:06:43 +0200

    Proyectos

---
+++ Curso02/MEAN/01_Usuarios/Angular/src/app/componentes/maquetacion/maquetacion-tienda/maquetacion-tienda.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maquetacion-tienda',
+  standalone: true,
+  imports: [],
+  templateUrl: './maquetacion-tienda.component.html'
+})
+export class MaquetacionTiendaComponent {
+
+}
---
+++ Curso02/MEAN/01_Usuarios/Angular/src/app/componentes/usuarios/confirmacion-registro/confirmacion-registro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmacion-registro',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirmacion-registro.component.html'
+})
+export class ConfirmacionRegistroComponent {
+
+}
